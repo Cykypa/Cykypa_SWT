3a34800ec9a16cd908c531a16c55d9ec79dc51f0
Cykypa <105830892+Cykypa@users.noreply.github.com>
2023-05-10 16:04:25 +0800

    Update dbinfo.py

--- 登录验证/db/dbinfo.py
+++ 登录验证/db/dbinfo.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 # 创建对象的基类:
 Base = declarative_base()
 # 初始化数据库连接:
-engine = create_engine('mysql+pymysql://root:111925ly@124.70.106.92:3306/Liu')  # 公司的服务器 MySQL 链接
+engine = create_engine('mysql+pymysql://root:密码@地址:端口/数据库名')  
 # engine = create_engine(
 #     'sqlite:///./config/sandian.db?check_same_thread=False')  # SQLITE 链接,使用SQLITE必须吧check_same_thread改为false
 # 创建DBSession类型:
